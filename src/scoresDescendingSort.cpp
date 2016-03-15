7a10beea89a31b4207af54b1e8826bcdabe35083
RajsekharKommala <RajsekharKommala@missionrnd.com>
2016-03-14 22:13:20 -0700

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -21,5 +21,20 @@ struct student {
 };
 
 void * scoresDescendingSort(struct student *students, int len) {
+	int i, j;
+	if (len<0 || students == NULL)
+		return NULL;
+	for (i = 0; i < len; i++)
+	{
+		for (j = i + 1; j < len; j++)
+		{
+			if (students[i].score < students[j].score)
+			{
+				students[i].score += students[j].score;
+				students[j].score = students[i].score - students[j].score;
+				students[i].score -= students[j].score;
+			}
+		}
+	}
 	return NULL;
 }
